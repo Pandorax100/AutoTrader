e9c751f739e7f4707459d962243b6512ae9b9f49
Liam McDermott <44736094+Pandorax100@users.noreply.github.com>
2022-05-17 18:24:05 +0100

    Update get stock method to use all URL parameters available in AutoTrader documentation

--- src/Pandorax.AutoTrader/AutoTraderService.cs
+++ src/Pandorax.AutoTrader/AutoTraderService.cs
@@ -1,6 +1,8 @@
+using System.Collections.Specialized;
 using System.Net.Http.Headers;
 using System.Text.Json;
 using System.Text.Json.Serialization;
+using System.Web;
 using Microsoft.Extensions.Options;
 using Pandorax.AutoTrader.Constants;
 using Pandorax.AutoTrader.Models;
@@ -44,21 +46,72 @@ namespace Pandorax.AutoTrader
             return JsonSerializer.Deserialize<AutoTraderNotification>(json, JsonSerializerOptions);
         }
 
-        public async Task<StockListResult?> GetStockByAdvertiserIdAsync(string advertiserId)
+        /// <inheritdoc />
+        public async Task<StockListResult?> GetStockAsync(
+            string? advertiserId = null,
+            int? pageSize = null,
+            int? page = null,
+            LifecycleState? lifecycleState = null,
+            string? searchId = null,
+            string? stockId = null,
+            string? registration = null,
+            string? vin = null)
         {
-            if (advertiserId is null)
-            {
-                throw new ArgumentNullException(nameof(advertiserId));
-            }
-
             string? accessToken = await GetAccessTokenAsync();
             _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
 
-            string? json = await _client.GetStringAsync($"/service/stock-management/stock?advertiserId={advertiserId}");
+            var query = BuildStockQueryString(advertiserId, pageSize, page, lifecycleState, searchId, stockId, registration, vin);
+
+            var url = string.Concat($"/service/stock-management/stock", query.Count > 0 ? "?" : null, query);
+
+            string json = await _client.GetStringAsync(url);
 
             StockListResult? parsed = JsonSerializer.Deserialize<StockListResult>(json, JsonSerializerOptions);
 
             return parsed;
+        }
+
+        private static NameValueCollection BuildStockQueryString(
+          string? advertiserId,
+          int? pageSize,
+          int? page,
+          LifecycleState? lifecycleState,
+          string? searchId,
+          string? stockId,
+          string? registration,
+          string? vin)
+        {
+            Dictionary<string, string?> queryString = new()
+            {
+                ["advertiserId"] = advertiserId,
+                ["pageSize"] = pageSize?.ToString(),
+                ["page"] = page?.ToString(),
+                ["lifecycleState"] = lifecycleState switch
+                {
+                    LifecycleState.Deleted => "DELETED",
+                    LifecycleState.Forecourt => "FORECOURT",
+                    LifecycleState.SaleInProgress => "SALE_IN_PROGRESS",
+                    LifecycleState.DueIn => "DUE_IN",
+                    LifecycleState.Wastebin => "WASTEBIN",
+                    _ => null,
+                },
+                ["searchId"] = searchId,
+                ["stockId"] = stockId,
+                ["registration"] = registration,
+                ["vin"] = vin,
+            };
+
+            NameValueCollection query = HttpUtility.ParseQueryString(string.Empty);
+
+            foreach (var (key, value) in queryString)
+            {
+                if (value is not null)
+                {
+                    query.Add(key, value);
+                }
+            }
+
+            return query;
         }
 
         private async Task<string?> GetAccessTokenAsync()
@@ -75,9 +128,9 @@ namespace Pandorax.AutoTrader
 
             string json = await response.Content.ReadAsStringAsync();
 
-            JsonElement parsed = (JsonElement)JsonSerializer.Deserialize<object>(json);
+            JsonElement? parsed = (JsonElement?)JsonSerializer.Deserialize<object>(json);
 
-            return parsed.GetProperty("access_token").GetString();
+            return parsed?.GetProperty("access_token").GetString();
         }
     }
 }
--- src/Pandorax.AutoTrader/IAutoTraderService.cs
+++ src/Pandorax.AutoTrader/IAutoTraderService.cs
@@ -1,10 +1,41 @@
 using Pandorax.AutoTrader.Models;
 
-namespace Pandorax.AutoTrader
+namespace Pandorax.AutoTrader;
+
+public interface IAutoTraderService
 {
-    public interface IAutoTraderService
-    {
-        AutoTraderNotification? ParseNotificationJson(string json);
-        Task<StockListResult> GetStockByAdvertiserIdAsync(string advertiserId);
-    }
+    AutoTraderNotification? ParseNotificationJson(string json);
+
+    /// <summary>
+    /// Retreives stock records and optionally searches by the given parameters.
+    /// </summary>
+    /// <param name="advertiserId">An optional advertiser id to return stock associated to a specific advertiser.</param>
+    /// <param name="pageSize">
+    /// Use this to adjust the number of results returned per page.
+    /// 20 is the default and recommended however you may request up to 200.
+    /// </param>
+    /// <param name="page">
+    /// <para>Used to request subsequten pages of results.</para>
+    /// <para>Pagination volume can be calculated by totalResults / pageSize.</para>
+    /// </param>
+    /// <param name="lifecycleState">
+    /// Used to retreive stock with a specific lifecycle state.
+    /// </param>
+    /// <param name="searchId">Used for a direct call to the stock records details by search Id.</param>
+    /// <param name="stockId">Used for a direct call to the stock records details by stock Id.</param>
+    /// <param name="registration">The vehicle registration number.</param>
+    /// <param name="vin">The VIN of the vehicle.</param>
+    /// <returns>
+    /// A tast representing the asynchronous operation. The result of the task is an object containing the total number
+    /// of records and a list of the records on the current page.
+    /// </returns>
+    Task<StockListResult?> GetStockAsync(
+        string? advertiserId = null,
+        int? pageSize = null,
+        int? page = null,
+        LifecycleState? lifecycleState = null,
+        string? searchId = null,
+        string? stockId = null,
+        string? registration = null,
+        string? vin = null);
 }
