f5a791a42d7736bae08b5fe1e9eec59cfa4951a4
Liam McDermott <44736094+Pandorax100@users.noreply.github.com>
2022-08-21 16:27:33 +0100

    Remove Ownership Condition Converter

--- src/Pandorax.AutoTrader/Api/Stock/Common/OwnershipCondition.cs
+++ src/Pandorax.AutoTrader/Api/Stock/Common/OwnershipCondition.cs
@@ -2,7 +2,7 @@ using Newtonsoft.Json;
 using Pandorax.AutoTrader.Converters;
 
 namespace Pandorax.AutoTrader.Api.Stock.Common;
-[JsonConverter(typeof(OwnershipConditionConverter))]
+
 public enum OwnershipCondition
 {
     New,
